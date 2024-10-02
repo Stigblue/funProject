8e7c0983cb9d8f0b1fd2b5e0d3f297611b5126b2
Stig von der Ahé <stigblue77@gmail.com>
2024-10-02 17:29:24 +0200

    update

--- Funproject/Funproject/CoreDataManager.swift
+++ Funproject/Funproject/CoreDataManager.swift
@@ -12,39 +12,40 @@ import CoreData
 // Interface Segregation Principle: The CoreDataManager class handles all CoreData operations, keeping CoreData logic segregated from the UI.
 @objc class CoreDataManager: NSObject {
     @objc static let shared = CoreDataManager()
-    
+
     public override init() {}
-    
+
     let persistentContainer: NSPersistentContainer = {
-        let container = NSPersistentContainer(name: "Funproject")
-        container.loadPersistentStores { _, error in
+        let container = NSPersistentContainer(name: "Funproject") // Use your actual .xcdatamodeld name
+        container.loadPersistentStores { description, error in
             if let error = error as NSError? {
                 fatalError("Unresolved error \(error), \(error.userInfo)")
             }
         }
         return container
     }()
-    
+
     func saveCheckInDate(_ date: Date) {
         let context = persistentContainer.viewContext
+
         let employee = Employee(context: context)
         let formatter = DateFormatter()
         formatter.dateFormat = "yyyy-MM-dd HH:mm"
         employee.check_in_date_time = formatter.string(from: date)
-        
+
         do {
             try context.save()
         } catch {
             print("Failed to save: \(error)")
         }
     }
-    
-   @objc func fetchInitialDate() -> Date {
+
+    @objc func fetchInitialDate() -> Date {
         let context = persistentContainer.viewContext
         let fetchRequest: NSFetchRequest<Employee> = Employee.fetchRequest()
         fetchRequest.sortDescriptors = [NSSortDescriptor(key: "check_in_date_time", ascending: false)]
         fetchRequest.fetchLimit = 1
-        
+
         do {
             let result = try context.fetch(fetchRequest).first
             if let dateString = result?.check_in_date_time {
@@ -57,7 +58,7 @@ import CoreData
         }
         return Date()
     }
-    
+
     @objc func fetchCompanyName() -> String? {
         let context = persistentContainer.viewContext
         let fetchRequest: NSFetchRequest<Company> = Company.fetchRequest()
